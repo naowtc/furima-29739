dc0b1998f3d315fce17e7bd7e24144ecfc88ec84
naowtc <naowtc@gmail.com>
2020-09-23 15:44:41 +0900

    deviseのインストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
 
 
-  # private
+  private
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'admin' && password == '2222'
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
