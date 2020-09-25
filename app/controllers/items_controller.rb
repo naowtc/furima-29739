b870ee6c57f65af216acdeb1835073c8c95e521d
naowtc <naowtc@gmail.com>
2020-09-25 18:28:00 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  # indexアクション以外のアクションが動く前にユーザーがログインしていなければルートページが表示されるようになる
+  before_action :authenticate_user! ,except: [:index]
+  
+
   def index
     
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  with_options presence: true do
+   validates :nickname
+   validates :birth_date
+   
+                     
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
+   
+
+   with_options format:{ with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." } do
+    validates :first_name
+    validates :last_name
+   end
+
+   with_options format:{ with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." } do
+    validates :first_name_kana
+    validates :last_name_kana
+   end
+  end 
 end
+
+   
