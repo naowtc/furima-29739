5f24921479c1b32bfa1b19497e7b610ae6db33a6
naowtc <naowtc@gmail.com>
2020-09-27 19:39:31 +0900

    出品ページへの遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
   # indexアクション以外のアクションが動く前にユーザーがログインしていなければルートページが表示されるようになる
   before_action :authenticate_user!, except: [:index]
-
+  
   def index
   end
+
+  def new
+    @item = Item.new
+  end
 end
+
