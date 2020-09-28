c2dee11e02285e1eed9dfcbac5cb5a0aa27425b5
naowtc <naowtc@gmail.com>
2020-09-28 19:15:31 +0900

    非同期通信(JavaScript)での金額設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,9 +19,10 @@ class Item < ApplicationRecord
   validates :postage_payer
   validates :prefecture 
   validates :shipment_time
-  # 金額範囲に関するバリデーション
-  validates :price , numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, with: /\A[0-9]+\z/}
-  end
+  validates :price
+end
+# 金額範囲に関するバリデーション
+validates :price , numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, with: /\A[0-9]+\z/}
   #選択が「--」のままになっていないか
   validates :category_id, :condition_id, :postage_payer_id, :prefecture_id, :prefecture_id, :shipment_time_id,numericality: { other_than: 0 }
 
