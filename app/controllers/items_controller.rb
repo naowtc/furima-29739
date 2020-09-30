258e7f9a9817c7a6883217f64c8d1e98d2e95554
naowtc <naowtc@gmail.com>
2020-09-30 11:31:10 +0900

    コンフリクト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
-  # indexアクション以外のアクションが動く前にユーザーがログインしていなければルートページが表示されるようになる
-  before_action :authenticate_user!, except: [:index]
+  # indexアクション以外のアクションが動く前にユーザーがログインしていなければルートページが表示されるようになる。exceptで特定のアクションを許可する
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -21,19 +21,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  # itemsコントローラーにshowアクションを定義。editアクションでは編集したいレコードを@itemに代入し、ビューに受け渡すことで編集画面で利用できる
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def edit
     # itemsコントローラーにeditアクションを定義。editアクションでは編集したいレコードを@itemに代入し、ビューに受け渡すことで編集画面で利用できる
     @item = Item.find(params[:id])
-    
   end
-    #  itemsコントローラーにupdateアクションを定義。正常に編集が完了した時とそうでない時で条件分岐
+
+  #  itemsコントローラーにupdateアクションを定義。正常に編集が完了した時とそうでない時で条件分岐
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
-    end 
+    end
   end
 
   private
